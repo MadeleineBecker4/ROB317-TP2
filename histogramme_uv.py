17d71fa2ba98b575240c8f2190e842f3aed1f1f0
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2020-11-09 15:50:26 +0100

    Comparaison d'histogrammes et hisotgramme agrandi pour etre lisible

--- histogramme_uv.py
+++ histogramme_uv.py
@@ -11,12 +11,13 @@ import time
 
 filename = 'Extrait1-Cosmos_Laundromat1(340p).m4v'
 #filename = 'Rotation_OY(Pan).m4v'
-directory = '../TP2_Videos_Exemples/'
-#directory = './TP2_Videos/
+#directory = '../TP2_Videos_Exemples/'
+directory = './TP2_Videos/'
 nVal = 256
 #nValU = nVal
 #nValV = nVal
-histogrammeUV = np.zeros((nVal,nVal))
+histUV = np.zeros((nVal,nVal))
+histUV_old = np.zeros((nVal,nVal))
 cap = cv2.VideoCapture(directory+filename)
 
 ret,frame = cap.read()
@@ -26,23 +27,45 @@ while(ret):
     frame_Yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
     #frame_uv = frame_Yuv[:,:,1:]
     
-    histogrammeUV = cv2.calcHist([frame_Yuv],[1,2], None, [256,256], [0,256,0,256])
+    histUV = cv2.calcHist([frame_Yuv],[1,2], None, [256,256], [0,256,0,256])
+    histShape = np.shape(histUV)
+    HumanVisibleHistogrammeUV = np.zeros((histShape[0]*3,histShape[1]*3))
+    HumanVisibleHistogrammeUV[0::3,0::3] = histUV
+    HumanVisibleHistogrammeUV[0::3,1::3] = histUV
+    HumanVisibleHistogrammeUV[0::3,2::3] = histUV
+    HumanVisibleHistogrammeUV[1::3,0::3] = histUV
+    HumanVisibleHistogrammeUV[1::3,1::3] = histUV
+    HumanVisibleHistogrammeUV[1::3,2::3] = histUV
+    HumanVisibleHistogrammeUV[2::3,0::3] = histUV
+    HumanVisibleHistogrammeUV[2::3,1::3] = histUV
+    HumanVisibleHistogrammeUV[2::3,2::3] = histUV
     
-    cv2.imshow('test affichage',frame_Yuv)
-    cv2.imshow('histogramme',histogrammeUV/(histogrammeUV.max()-histogrammeUV.min()))
+    cv2.imshow('test affichage',frame)
+    #cv2.imshow('histogramme',histUV/(histUV.max()-histUV.min()))
+    cv2.imshow('histogramme visible by human',HumanVisibleHistogrammeUV/(HumanVisibleHistogrammeUV.max()-HumanVisibleHistogrammeUV.min()))
     
+    if index>0:
+        distance = cv2.compareHist(histUV,histUV_old,cv2.HISTCMP_CORREL)
+        if distance <0.3:
+            print(distance)
+            cv2.imwrite('image%04d.png'%index,frame)
+
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
     elif k == ord('s'):
         cv2.imwrite('image%04d.png'%index,frame)
-        cv2.imwrite('hist%04d.png'%index,histogrammeUV)
+        cv2.imwrite('hist%04d.png'%index,histUV)
     elif k == ord('p'):
         while k != ord('g'):
             time.sleep(1)
             k = cv2.waitKey(30) & 0xff
     index += 1
+    histUV_old = histUV
     ret,frame = cap.read()
+    if (frame==[]):
+        print("The end")
 
 
 cv2.destroyAllWindows()
